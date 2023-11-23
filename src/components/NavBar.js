099b652bd5f890a60525a770a004b343f072b7f4
hillary-wangui <hillary.maina@student.moringaschool.com>
2023-11-23 23:50:05 +0300

    assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,18 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+ const LinksMenu = links.map(link => {
+  return(
+    <a href={link} key={link}>{link}</a>
+  )
+ })
+  return (
+  <>
+  <nav>{/* display an <a> tag for each link here */}
+  {LinksMenu}
+  </nav>
+  </>
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+)}
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
 function ProjectList({ projects }) {
   console.log(projects);
+const ProjectLists = projects.map(items => {
+  return ((
+    <ProjectItem
+      key={items.id}
+      name={items.name}
+      about={items.about}
+      technologies={items.technologies}
+    />
+    ))
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ProjectLists}</div>
     </div>
   );
 }
